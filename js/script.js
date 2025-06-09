c1a4bd88fda9aed4ddf2fa5f061ca1ec5dd77177
skanthiwar <skanthiwar1@gmail.com>
2025-06-09 21:25:24 +0530

    updated the featured project

--- js/script.js
+++ js/script.js
@@ -42,7 +42,30 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Featured Project
+const modals = [
+    { trigger: ".popup-trigger-1", popup: "#popup-project-1", close: ".popup-close-1" },
+    { trigger: ".popup-trigger-2", popup: "#popup-project-2", close: ".popup-close-2" },
+    { trigger: ".popup-trigger-3", popup: "#popup-project-3", close: ".popup-close-3" },
+    { trigger: ".popup-trigger-4", popup: "#popup-project-4", close: ".popup-close-4" },
+    { trigger: ".popup-trigger-5", popup: "#popup-project-5", close: ".popup-close-5" },
+    { trigger: ".popup-trigger-6", popup: "#popup-project-6", close: ".popup-close-6" },
+  ];
 
+  modals.forEach(({ trigger, popup, close }) => {
+    document.querySelector(trigger).addEventListener("click", () => {
+      document.querySelector(popup).style.display = "block";
+    });
+
+    document.querySelector(close).addEventListener("click", () => {
+      document.querySelector(popup).style.display = "none";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === document.querySelector(popup)) {
+        document.querySelector(popup).style.display = "none";
+      }
+    });
+  });
 
 // Projects Page
 // Modal Logic
